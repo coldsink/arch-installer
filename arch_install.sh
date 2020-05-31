73ba19006c6da80ad3ea960cfe11bdcfab67d2f4
coldsink <62737133+coldsink@users.noreply.github.com>
2020-05-30 22:39:04 -0700

    Add files via upload

---
+++ arch_install.sh
@@ -0,0 +1,163 @@
+#! /bin/bash
+prompt="echo "=================================================================================""
+clear
+$prompt
+echo
+echo " > Starting install..."
+echo
+$prompt
+sleep 0.5s
+clear
+$prompt
+echo
+echo " > Enter "yes" for wireless. Enter "no" for wired. "
+echo
+$prompt
+echo
+read -p " -> " network
+	if [ $network = yes ]
+	then
+		clear
+		wifi-menu
+		clear
+	else
+		clear
+		$prompt
+		echo
+		ip link
+		echo
+		$prompt
+		echo
+		echo " > Select wired interface. "
+		read -p " -> " interface
+		ip link set up $interface
+		sleep 2s
+		clear
+	fi
+		timedatectl set-ntp true
+		echo " > Scanning drives... "
+		sleep 0.25s
+		clear
+		echo
+		$prompt
+		echo
+		echo " > Select drive to partition. This operation cannot be reverted. (ex. sdX, not /dev/sdX) "
+		echo
+		lsblk
+		echo
+		$prompt
+		echo
+		read -p ' -> ' drive
+		clear
+		$prompt
+		echo
+		echo " > Wiping drive... "
+		echo
+		$prompt
+		sleep 0.5s
+		wipefs -a /dev/$drive
+		clear
+		$prompt
+		echo
+		echo " > Create partitions... "
+		echo
+		$prompt
+		sleep 0.5s
+		cfdisk /dev/$drive
+		clear
+		$prompt
+		echo
+		echo " > Formatting partitions... "
+		echo
+		$prompt
+		sleep 1s
+		clear
+		$prompt
+		echo
+		lsblk
+		echo
+		$prompt
+		echo
+		echo " > Enter efi partition. "
+		read -p " -> " efi
+		mkfs.vfat -n EFI /dev/$efi
+		clear
+		$prompt
+		echo
+		lsblk
+		echo
+		$prompt
+		echo
+		echo " > Enter swap partition. "
+		read -p " -> " swp 
+		mkswap -L SWAP /dev/$swp
+		clear
+		$prompt
+		echo
+		lsblk
+		echo
+		$prompt
+		echo
+		echo " > Enter root partition. "
+		read -p " -> " root 
+		mkfs.ext4 -L ROOT /dev/$root
+		clear
+		$prompt
+		echo
+		lsblk
+		echo
+		$prompt
+		echo
+		echo " > Enter home partition. "
+		read -p " -> " home 
+		mkfs.ext4 -L HOME /dev/$home
+		clear
+		$prompt
+		echo
+		echo " > Mounting partitions... "
+		echo
+		$prompt
+		sleep 0.5s
+		mount /dev/$root /mnt
+		mkdir /mnt/home
+		mount /dev/$home /mnt/home
+		mkdir /mnt/efi
+		mount /dev/$efi /mnt/efi
+		swapon /dev/$swp
+		clear
+		$prompt
+		echo
+		echo " > Edit mirrorlist... "
+		echo
+		$prompt
+		sleep 1s
+		vim /etc/pacman.d/mirrorlist
+		clear
+		$prompt
+		echo
+		echo " > Installing base system... "
+		echo
+		$prompt
+		echo
+		pacstrap /mnt base linux linux-firmware base-devel vim sudo wpa_supplicant dhcpcd
+		clear
+		$prompt
+		echo
+		echo " > Generating fstab... "
+		echo
+		$prompt
+		genfstab -U /mnt >> /mnt/etc/fstab
+		sleep 0.5s
+		cp arch_install_2.sh /mnt
+		clear
+		$prompt
+		echo
+		echo " > Chrooting... "
+		echo
+		$prompt
+		sleep 0.5s
+		clear
+		arch-chroot /mnt ./arch_install_2.sh
+		umount -R /mnt
+		reboot
+
---
+++ arch_install_2.sh
@@ -0,0 +1,150 @@
+#! /bin/bash
+
+prompt="echo "======================================================================"
+clear
+$prompt
+echo
+echo " > Setting zoneinfo... "
+echo
+$prompt
+ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+sleep 0.5s
+clear
+$prompt
+echo
+echo " > Finalizing locale preferences. " 
+echo
+$prompt
+echo "en_US.UTF-8 UTF-8 > /etc/locale.gen"
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+locale-gen
+hwclock --systohc --utc
+clear
+$prompt
+echo
+echo " > Please enter machine hostname. "
+echo
+$prompt
+echo
+read -p ' -> ' hostname
+clear
+$prompt
+echo
+echo " > Hostname set. "
+echo
+$prompt
+echo "$hostname" >> /etc/hostname
+echo "
+127.0.0.1	localhost
+::1		localhost
+127.0.1.1 	$hostname.localdomain	$hostname" >> /etc/hosts
+sleep 0.5s
+clear
+$prompt
+echo
+echo " > Configuring sudo... "
+echo
+$prompt
+sleep 0.5s
+clear
+visudo
+clear
+$prompt
+echo
+echo " > Installing bootloader... "
+echo
+$prompt
+sleep 0.5s
+clear
+$prompt
+echo
+echo " > Installing bootloader... "
+echo
+$prompt
+echo
+pacman -S grub efibootmgr
+clear
+$prompt
+echo
+echo " > Configuring bootloader... " 
+echo
+$prompt
+sleep 0.5s
+clear
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=arch
+clear
+grub-mkconfig -o /boot/grub/grub.cfg
+clear
+$prompt
+echo
+echo " > Set root password. "
+echo
+$prompt
+echo
+passwd
+clear
+sleep 0.5s
+$prompt
+echo
+echo " > Enter "yes" for wireless. Enter "no" for wired. "
+echo
+$prompt
+echo
+read -p " -> " network
+	if [ $network = yes ]
+	then 
+		clear
+		$prompt
+		echo
+		ip link 
+		echo
+		$prompt
+		echo
+		echo " > Choose wireless interface. "
+		read -p " -> " wireless_interface
+		echo
+		echo " > Enter network SSID. "
+		read -p " -> " ssid
+		echo
+		echo " > Enter network password. "
+		read -p " -> " psk
+		clear
+		$prompt
+		echo
+		echo " > Settings applied. Configuring network settings... "
+		echo
+		$prompt
+		mkdir -p /etc/wpa_supplicant 
+		wpa_passphrase "$ssid" "$psk" >> /etc/wpa_supplicant/wpa_supplicant-$wireless_interface.conf
+		clear
+		$prompt
+		echo
+		echo " > Upon reboot, you will need to enable dhcpcd@$wireless_interface.service and wpa_supplicant@$wireless_interface.service. "
+		echo
+		$prompt
+		sleep 3s
+	else
+		clear
+		$prompt
+		echo
+		ip link
+		echo
+		$prompt
+		echo
+		echo " > Choose wired interface. "	
+		read -p " -> " wired_interface
+		clear
+		$prompt
+		echo
+		echo " > Upon reboot, you will need to enable dhcpcd@$wired_interface.service "
+		echo
+		$prompt
+		sleep 3s
+		clear
+$prompt
+echo
+echo "Installation finished. Rebooting"
+echo
+$prompt
+sleep 1s
+clear
